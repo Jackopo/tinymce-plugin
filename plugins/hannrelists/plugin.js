5881be2dca2a4eaca31226a95143d60975d702ae
Jacopo Ghisolfi <jacopo.ghisolfi@infopark.de>
2015-10-13 14:05:34 +0200

    Initial commit with basic tinymce setup - tinymce version 4.1.7

---
+++ plugins/hannrelists/plugin.js
@@ -0,0 +1,11 @@
+tinymce.PluginManager.add('hannrelists', function(editor, url) {
+
+
+  editor.addButton('hannrelists', {
+		text: 'HR lists',
+		icon: true,
+		onclick: function() {
+
+		}
+	});
+});
