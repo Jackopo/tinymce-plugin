97bdedfff39039e2ee0dac67d24e926e267cddc1
Jacopo Ghisolfi <jacopo.ghisolfi@infopark.de>
2015-10-13 14:06:54 +0200

    Added index.html and main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+tinymce.init({
+  selector: "#mytextarea",
+  mode: "specific_textareas",
+  // editor_selector: "mceEditor",
+  plugins: "paste searchreplace table code anchor visualblocks hannrelists",
+  dialog_type: "modal",
+  entity_encoding : "raw",
+  toolbar1: "bold italic underline strikethrough | hannrelists bullist numlist outdent  indent | formatselect fontselect fontsizeselect",
+  toolbar2: "undo redo | searchreplace | removeformat visualblocks | anchor | table | superscript subscript | alignleft aligncenter alignright alignjustify | code"
+});
