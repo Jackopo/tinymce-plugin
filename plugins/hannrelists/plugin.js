b7f8f9a0809001ad0ac6c5de37980237b45ddd01
Jacopo Ghisolfi <jacopo.ghisolfi@infopark.de>
2015-10-13 17:15:41 +0200

    implemented basic feature to add a list with links. The list has a customized class

--- plugins/hannrelists/plugin.js
+++ plugins/hannrelists/plugin.js
@@ -1,17 +1,49 @@
 tinymce.PluginManager.add('hannrelists', function(editor, url) {
 
-  function bindSelectorSetContent() {
-    var selection = editor.selection;
-    selection.setContent("Hello World!");
-	}
+  function createLinkTag(url, text) {
+    return "<a href='" + url + "' class='link'>" + text + "</a>";
+  }
 
-	editor.addButton('hannrelists', {
-		text: 'HR lists',
-		icon: true,
-		onclick: function() {
 
-      bindSelectorSetContent();
 
+  editor.addButton('hannrelists', {
+    text: 'HR lists',
+    icon: true,
+    onclick: function() {
+
+      // Open window
+      editor.windowManager.open({
+          title: 'Hannre Download List',
+          body: [
+              {type: 'textbox', name: 'link', label: 'Name'},
+              {type: 'textbox', name: 'url', label: 'URL'}
+          ],
+          onsubmit: function(e) {
+            //
+            var list;
+            var dom = editor.dom;
+            var selection = editor.selection;
+
+            // Check for existing list element
+            list = dom.getParent(selection.getNode(), 'ul');
+
+            // Switch/add list type if needed
+            if (!list) {
+              editor.execCommand('InsertUnorderedList');
+            }
+
+            list = dom.getParent(selection.getNode(), 'ul');
+
+            if (list) {
+              dom.addClass(list, "myclass");
+            }
+
+            var link = createLinkTag(e.data.url, e.data.link);
+            editor.insertContent(link);
+
+            link.removeAttribute('data-mce-href');
+          }
+      });
 		}
 	});
 });
