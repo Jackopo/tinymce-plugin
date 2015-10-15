06991a95f7a003e9aa7d5eb21db3170096f795c6
Jacopo Ghisolfi <jacopo.ghisolfi@infopark.de>
2015-10-15 10:10:56 +0200

    code has been refactored and wrapped in an anonymous function

--- plugins/hannrelists/plugin.js
+++ plugins/hannrelists/plugin.js
@@ -1,36 +1,54 @@
-tinymce.PluginManager.add('hannrelists', function(editor, url) {
 
-  editor.addButton('hannrelists', {
-    text: 'HR lists',
-    icon: true,
-    onclick: function() {
+// longname: 'Hannre Lists Plugin',
+// author: 'Infopark AG',
+// authorurl: 'http://www.infopark.de',
+// infourl: 'http://www.infopark.de',
+// version: "0.2"
 
-      var list;
-      var dom = editor.dom;
-      var selection = editor.selection;
-      var domQuery = tinymce.dom.DomQuery;
 
-      // Check for existing list element
+
+(function() {
+  tinymce.PluginManager.add('hannrelists', function(editor, url) {
+    function createList(list, editor, dom, domQuery, selection) {
+
+
+      editor.execCommand('InsertUnorderedList');
       list = dom.getParent(selection.getNode(), 'ul');
+      dom.addClass(list, "download-link-list");
+      var item = dom.getParent(selection.getNode(), 'li');
 
-      // Switch/add list type if needed
-      if (!list) {
-        editor.execCommand('InsertUnorderedList');
-        list = dom.getParent(selection.getNode(), 'ul');
-        dom.addClass(list, "download-link-list");
-        var item = dom.getParent(selection.getNode(), 'li');
-
-        while (item) {
-          dom.addClass(item, "item");
-          var subItem = domQuery.find("a", item);
-          dom.addClass(subItem, "link");
-          item = dom.getPrev(item, 'li');
-        }
-      }
-
-      if (list) {
-        // add items to a list
+      while (item) {
+        dom.addClass(item, "item");
+        var subItem = domQuery.find("strong", item);
+        dom.addClass(subItem, "link");
+        item = dom.getPrev(item, 'li');
       }
     }
+
+    function findLists() {
+        var list;
+        var dom = editor.dom;
+        var selection = editor.selection;
+        var domQuery = tinymce.dom.DomQuery;
+
+        // Check for existing list element
+        list = dom.getParent(selection.getNode(), 'ul');
+
+        // Add ul type if needed
+        if (!list) {
+          createList(list, editor, dom, domQuery, selection);
+        }
+
+        if (list) {
+          // if a list is already present
+        }
+    }
+
+
+    editor.addButton('hannrelists', {
+      text: 'HR lists',
+      icon: true,
+      onclick: findLists
+    });
   });
-});
+}());
