0702fbe56f083d39d848f7613d852177aeb9bdfa
Jacopo Ghisolfi <jacopo.ghisolfi@infopark.de>
2015-10-15 16:06:34 +0200

    Two types of lists can be added now

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ tinymce.init({
   plugins: "paste searchreplace table code anchor visualblocks hannrelists npsfilebrowser ",
   dialog_type: "modal",
   entity_encoding : "raw",
-  toolbar1: "bold italic underline strikethrough | hannrelists bullist numlist outdent  indent | formatselect fontselect fontsizeselect",
+  toolbar1: "bold italic underline strikethrough | hannrelistsDownload hannrelistsLinks bullist numlist outdent  indent | formatselect fontselect fontsizeselect",
   toolbar2: "undo redo | searchreplace | removeformat visualblocks | anchor npsLinkBrowser npsImageBrowser | table | superscript subscript | alignleft aligncenter alignright alignjustify | code",
   content_css: "css/style.css"
 });
--- plugins/hannrelists/plugin.js
+++ plugins/hannrelists/plugin.js
@@ -9,13 +9,20 @@
 
 (function() {
   tinymce.PluginManager.add('hannrelists', function(editor, url) {
+
+    var linkType =  {
+      download: "link",
+      links: "link arrow-down"
+    };
+
+
     function createList(editor, dom, selection) {
 
       editor.execCommand('InsertUnorderedList');
       return dom.getParent(selection.getNode(), 'ul');
     }
 
-    function convertList(list, editor, dom, domQuery, selection) {
+    function convertList(list, editor, dom, domQuery, selection, linkTypeName) {
 
       dom.addClass(list, "download-link-list");
       var items = domQuery.find('li', list);
@@ -23,36 +30,46 @@
       domQuery.each(items, function(index, item) {
         dom.addClass(item, "item");
         var subItem = domQuery.find("strong", item);
-        dom.addClass(subItem, "link");
+        dom.addClass(subItem, linkType[linkTypeName]);
         item = dom.getPrev(item, 'li');
       });
     }
 
-    function findLists() {
-        var list;
-        var dom = editor.dom;
-        var selection = editor.selection;
-        var domQuery = tinymce.dom.DomQuery;
+    function findLists(linkTypeName) {
+      var list;
+      var dom = editor.dom;
+      var selection = editor.selection;
+      var domQuery = tinymce.dom.DomQuery;
 
-        // Check for existing list element
-        list = dom.getParent(selection.getNode(), 'ul');
+      // Check for existing list element
+      list = dom.getParent(selection.getNode(), 'ul');
 
-        // Add ul type if needed
-        if (!list) {
-          list = createList(editor, dom, selection);
-        }
+      // Add ul type if needed
+      if (!list) {
+        list = createList(editor, dom, selection);
+      }
 
-        if (list) {
-          // if a list is already present
-          convertList(list, editor, dom, domQuery, selection);
-        }
+      if (list) {
+        // if a list is already present
+        convertList(list, editor, dom, domQuery, selection, linkTypeName);
+      }
     }
 
 
-    editor.addButton('hannrelists', {
-      text: 'HR lists',
+    editor.addButton('hannrelistsDownload', {
+      text: 'HR lists - Down',
       icon: true,
-      onclick: findLists
+      onclick: function(event) {
+        findLists('download')
+      }
+    });
+
+    editor.addButton('hannrelistsLinks', {
+      text: 'HR lists- Links',
+      icon: true,
+      onclick: function(event) {
+        findLists('links')
+      }
     });
   });
 }());
